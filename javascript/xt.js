aeaaa2e70e394548d26f1def3b0b161c389cb2d5
itzsam-lol <142300565+itzsam-lol@users.noreply.github.com>
2023-10-25 11:45:31 +0530

    Add files via upload

---
+++ javascript/xt.js
@@ -0,0 +1,25 @@
+// Set the target date for the countdown (October 22, 2023 at 00:00 AM)
+var targetDate = new Date("2023-10-27T00:00:00").getTime();
+
+var countdown = setInterval(function() {
+    var now = new Date().getTime();
+    var distance = targetDate - now;
+
+    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    document.getElementById("days").textContent = days.toString().padStart(2, '0');
+    document.getElementById("hours").textContent = hours.toString().padStart(2, '0');
+    document.getElementById("minutes").textContent = minutes.toString().padStart(2, '0');
+    document.getElementById("seconds").textContent = seconds.toString().padStart(2, '0');
+
+    if (distance < 0) {
+        clearInterval(countdown);
+        document.getElementById("days").textContent = "00";
+        document.getElementById("hours").textContent = "00";
+        document.getElementById("minutes").textContent = "00";
+        document.getElementById("seconds").textContent = "00";
+    }
+}, 1000);
